7871fe0f7b77b24a5c15deb3969ac45b01078089
JoyGitHubPlace <1175725736@qq.com>
2021-04-04 21:55:18 +0800

    readme.txt version:v0.3

---
+++ editorpath/Assets/editor/inspectorValue/setInspectorValueWithEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(inspectorValue))]
+public class setInspectorValueWithEditor : Editor {
+	inspectorValue item;
+	public override void OnInspectorGUI()
+	{
+		item = target as inspectorValue;
+		int width = EditorGUILayout.IntField("Width", item.width);   //编辑器对应的值重设为mono中类对象操作的值
+		if (item.width != width)
+		{
+			item.width = width;
+		}
+		base.DrawDefaultInspector();
+	}
+
+}
---
+++ editorpath/Assets/script/inspector/inspectorValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inspectorValue : MonoBehaviour {
+
+	public int width
+	{
+		get
+		{
+			return _width;
+		}
+		set
+		{
+			_width = value;
+		}
+	}
+	[SerializeField]
+	private int _width;
+}
