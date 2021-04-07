339f897a6ccac403866aee8203fca418c13a7e07
JoyGitHubPlace <1175725736@qq.com>
2021-04-07 22:35:09 +0800

    readme.txt v0.52

---
+++ editorpath/Assets/editor/ConsoleProgress/consoleProgressEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;  //必须引用的库
+using UnityEditor;
+
+public class consoleProgressEditor : Editor {
+
+	[MenuItem("myEditor/clearConsole")]
+	static void clearConsole() {
+		var assembly = Assembly.GetAssembly(typeof(UnityEditor.ActiveEditorTracker));  //已经封装好的库可以全局映射索引到对应类，获取对应方法名然后invoke执行
+		var type = assembly.GetType("UnityEditorInternal.LogEntries");
+		var method = type.GetMethod("Clear");
+		method.Invoke(new object(), null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
