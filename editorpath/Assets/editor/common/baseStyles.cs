6b22845c2d0df2ba84b31f0ee2529bc80e79fa7e
JoyGitHubPlace <1175725736@qq.com>
2021-04-07 23:12:43 +0800

    readme.txt v0.53

---
+++ editorpath/Assets/editor/common/baseStyles.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 查看默认的gui skin样式
+/// </summary>
+public class baseStyles : EditorWindow
+{
+    private Vector2 scrollPosition = Vector2.zero;
+    private string search = string.Empty;
+
+    [MenuItem("myEditor/baseEditor")]    //脑子不好使，整下来基于样式方便复制粘贴style名，哈哈哈
+    static void Init()
+    {
+        var window = EditorWindow.GetWindow<baseStyles>();
+        window.title = "baseEditor list";
+        window.Show();
+
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal("HelpBox");
+        GUILayout.Label("我是一个label", "label");
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("查找:");
+        search = EditorGUILayout.TextField(search);
+        GUILayout.EndHorizontal();
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+        foreach (GUIStyle style in GUI.skin)
+        {
+            //过滤
+            if (style.name.ToLower().Contains(search.ToLower()))
+            {
+                //设置奇偶行不同背景
+                GUILayout.BeginHorizontal("PopupCurveSwatchBackground");
+                GUILayout.Space(20);//左边留白20
+                if (GUILayout.Button(style.name, style))
+                {
+                    //把名字存储在剪粘板 
+                    EditorGUIUtility.systemCopyBuffer = style.name; 
+                }
+                GUILayout.FlexibleSpace();
+                EditorGUILayout.SelectableLabel("\"" + style.name + "\"");
+                GUILayout.EndHorizontal();
+                GUILayout.Space(20);//右边留白20
+            }
+        }
+
+        GUILayout.EndScrollView();
+    }
+}
---
+++ editorpath/Assets/editor/common/gmExample.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class gmExample : EditorWindow
+{
+
+    [MenuItem("myEditor/GM指令")]
+    static void Init()
+    {
+        var window = EditorWindow.GetWindow<gmExample>();
+        window.title = " GM指令";
+        window.Show();
+    }
+
+    private int newExp = 0, newMoney = 0, newVip = 0,  newCoin = 0;
+    private string uuid = "";
+    private int maxHp = 0, maxVp = 0, maxHurt = 0, newVp = 0, newSpirts = 0;
+
+    private int nMapId = 0;
+
+    public void OnGUI()
+    {
+        EditorGUILayout.LabelField("== 加数值 指令 ==");
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("用户id:");
+        uuid = EditorGUILayout.TextField(uuid, GUILayout.ExpandWidth(true), GUILayout.MinHeight(20));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("经验:");
+        newExp = EditorGUILayout.IntField(newExp, GUILayout.ExpandWidth(true), GUILayout.MinHeight(20));
+        if (GUILayout.Button("加经验", GUILayout.MinWidth(100), GUILayout.MaxHeight(20)))
+        {
+            if (uuid=="") {
+                Debug.LogWarning("uuid不能为空");
+            }
+            //AddExp(newExp,uuid);
+        }
+        //-------
+        GUILayout.Label("VIP钱:");
+        newVip = EditorGUILayout.IntField(newVip, GUILayout.ExpandWidth(true), GUILayout.MinHeight(20));
+        if (GUILayout.Button("加VIP", GUILayout.MinWidth(100), GUILayout.MaxHeight(20)))
+        {
+            if (uuid == "")
+            {
+                Debug.LogWarning("uuid不能为空");
+            }
+            //AddVip(newVip,uuid);
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("金币:");
+        newCoin = EditorGUILayout.IntField(newCoin);
+        if (GUILayout.Button("加金币", GUILayout.MinWidth(100), GUILayout.MaxHeight(20)))
+        {
+            if (uuid == "")
+            {
+                Debug.LogWarning("uuid不能为空");
+            }
+            //AddCoin(newCoin,uuid);
+        }
+        //-------
+        GUILayout.Label("元宝");
+        newMoney = EditorGUILayout.IntField(newMoney);
+        if (GUILayout.Button("加元宝", GUILayout.MinWidth(100), GUILayout.MaxHeight(20)))
+        {
+            if (uuid == "")
+            {
+                Debug.LogWarning("uuid不能为空");
+            }
+            //AddMoney(newMoney,uuid);
+        }
+        GUILayout.EndHorizontal();
+        //自主扩展
+    }
+}
